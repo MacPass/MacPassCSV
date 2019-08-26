ee17f231ae245aafcb3bc056d04c53e80b92111c
Michael Starke <michael.starke@hicknhack-software.com>
2019-08-26 11:28:31 +0200

    Updated git ignore

---
+++ MacPassCSV/MPCSVPlugin.h
@@ -0,0 +1,17 @@
+//
+//  MPCSVPlugin.h
+//  MacPassCSV
+//
+//  Created by Michael Starke on 26.08.19.
+//  Copyright © 2019 HicknHack Software GmbH. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface MPCSVPlugin : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ MacPassCSV/MPCSVPlugin.m
@@ -0,0 +1,13 @@
+//
+//  MPCSVPlugin.m
+//  MacPassCSV
+//
+//  Created by Michael Starke on 26.08.19.
+//  Copyright © 2019 HicknHack Software GmbH. All rights reserved.
+//
+
+#import "MPCSVPlugin.h"
+
+@implementation MPCSVPlugin
+
+@end
